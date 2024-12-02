8e13c37b0861c296eabc5c6900dd879742ca568e
Kishan Jasani <kishanjasani007@yahoo.in>
2024-12-02 23:12:07 +0530

    Setup supabase

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,3 +1,5 @@
+import {supabase, supacase} from '@/app/_lib/supabase';
+
 export async function getCountries() {
 	try {
 		const res = await fetch(
@@ -8,4 +10,17 @@ export async function getCountries() {
 	} catch {
 		throw new Error('Could not fetch countries');
 	}
+}
+
+export async function getCabin(id) {
+	const { data, error } = await supabase
+		.from("cabins")
+		.select("*")
+		.eq("id", id)
+		.single();
+
+	if ( error ) {
+		console.error(error);
+	}
+	return data;
 }
