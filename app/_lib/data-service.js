282b6336addfe320342fbc98882e133ce0091216
Kishan Jasani <kishanjasani007@yahoo.in>
2024-12-06 01:26:02 +0530

    Add delete selection and cabin booking form component - Fire multiple asyncronous request using Promise.all - Handle server component from client component - Fetch booking data for the particular cabin

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -20,6 +20,9 @@ export async function getCabin(id) {
 		.eq('id', id)
 		.single();
 
+	// For testing UI.
+	await new Promise((res) => setTimeout(res, 2000));
+
 	if (error) {
 		console.error(error);
 		notFound();
@@ -40,3 +43,53 @@ export const getCabins = async function () {
 
 	return data;
 };
+
+export async function getBookedDatesByCabinId(cabinId) {
+	let today = new Date();
+	today.setUTCHours(0, 0, 0, 0);
+	today = today.toISOString();
+
+	// For testing UI.
+	await new Promise((res) => setTimeout(res, 2000));
+
+	// Getting all bookings
+	const { data, error } = await supabase
+		.from('bookings')
+		.select('*')
+		.eq('cabinId', cabinId)
+		.or(`startDate.gte.${today},status.eq.checked-in`);
+
+	if (error) {
+		console.error(error);
+		throw new Error('Bookings could not get loaded');
+	}
+
+	// Converting to actual dates to be displayed in the date picker
+	const bookedDates = data
+		.map((booking) => {
+			return eachDayOfInterval({
+				start: new Date(booking.startDate),
+				end: new Date(booking.endDate),
+			});
+		})
+		.flat();
+
+	return bookedDates;
+}
+
+export async function getSettings() {
+	const { data, error } = await supabase
+		.from('settings')
+		.select('*')
+		.single();
+
+	// For testing UI.
+	await new Promise((res) => setTimeout(res, 5000));
+
+	if (error) {
+		console.error(error);
+		throw new Error('Settings could not be loaded');
+	}
+
+	return data;
+}
