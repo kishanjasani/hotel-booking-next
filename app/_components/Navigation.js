d8fb72f41acbdf98a532c5b5b4e5380e4b3357e6
Kishan Jasani <kishanjasani007@yahoo.in>
2024-12-14 01:41:14 +0530

    Fetch users session from the auth.js

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { auth } from '../_lib/auth';
 
-export default function Navigation() {
+export default async function Navigation() {
+	const session = await auth();
+
 	return (
 		<nav className="z-10 text-xl">
 			<ul className="flex gap-16 items-center">
@@ -29,12 +32,26 @@ export default function Navigation() {
 					</Link>
 				</li>
 				<li>
-					<Link
-						href="/contact"
-						className="hover:text-accent-400 transition-colors"
-					>
-						Contact us
-					</Link>
+					{session?.user?.image ? (
+						<Link
+							href="/contact"
+							className="hover:text-accent-400 transition-colors"
+						>
+							<img
+								className="h-8 rounded-full"
+								src={session.user.image}
+								alt={session.user.name}
+								referrerPolicy="no-referrer"
+							/>
+						</Link>
+					) : (
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors"
+						>
+							Guest Area
+						</Link>
+					)}
 				</li>
 			</ul>
 		</nav>
--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,8 +1,12 @@
+import { auth } from '../_lib/auth';
 import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
 import DateSelector from './DateSelector';
+import LoginMessage from './LoginMessage';
 import ReservationForm from './ReservationForm';
 
 export default async function Reservation({ cabin }) {
+	const session = await auth();
+
 	const [settings, bookedDates] = await Promise.all([
 		getSettings(),
 		getBookedDatesByCabinId(cabin.id),
@@ -15,7 +19,11 @@ export default async function Reservation({ cabin }) {
 				bookedDates={bookedDates}
 				cabin={cabin}
 			/>
-			<ReservationForm cabin={cabin} />
+			{session?.user ? (
+				<ReservationForm cabin={cabin} user={session.user} />
+			) : (
+				<LoginMessage />
+			)}
 		</div>
 	);
 }
