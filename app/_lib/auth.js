d7668f818d7f9a4c02b3e6522d723c657773530a
Kishan Jasani <kishanjasani007@yahoo.in>
2024-12-15 19:19:35 +0530

    Create user on supabase on successful G-Login

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { createGuest, getGuest } from './data-service';
 
 const authConfig = {
 	providers: [
@@ -11,6 +12,26 @@ const authConfig = {
 	callbacks: {
 		authorized({ auth, request }) {
 			return !!auth?.user;
+		},
+		async signIn(user) {
+			try {
+				const existingGuest = await getGuest(user.user.email);
+
+				if (!existingGuest) {
+					await createGuest({
+						email: user.user.email,
+						fullName: user.user.name,
+					});
+				}
+				return true;
+			} catch {
+				return false;
+			}
+		},
+		async session({ session, user }) {
+			const guest = getGuest(session.user.email);
+			session.user.guestId = guest.id;
+			return session;
 		},
 	},
 	pages: {
